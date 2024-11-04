cfc1714d73445ab0d848522abe629c195a3b7c92
root <root@BOOK-G99JFJOUSP>
2024-11-04 16:55:51 +0900

    11.2

--- src/vec3.h
+++ src/vec3.h
@@ -43,6 +43,12 @@ class vec3
             return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
         }
 
+        bool near_zero() const {
+            // Return true if the vector is close to zero in all dimension.
+            auto s = 1e-8;
+            return (std::fabs(e[0] < s) && std::fabs(e[1] < s) && std::fabs(e[2] < s));
+        }
+
         static vec3 random() {
             return vec3(random_double(), random_double(), random_double());
         }
@@ -72,7 +78,7 @@ inline vec3 operator-(const vec3& u, const vec3& v){
 }
 
 inline vec3 operator*(const vec3& u,const vec3& v){
-    return vec3(u.e[0]*v.e[0], u.e[1]*v.e[1], u.e[2]*u.e[2]);
+    return vec3(u.e[0]*v.e[0], u.e[1]*v.e[1], u.e[2]*v.e[2]);
 }
 
 inline vec3 operator*(double t, const vec3& v) {
@@ -114,10 +120,21 @@ inline vec3 random_unit_vector() {
 
 inline vec3 random_on_hemisphere(const vec3& normal) {
     vec3 on_unit_sphere = random_unit_vector();
-    if(dot(on_unit_sphere, normal) > 0.0) // In the same heisphere as the normal
+    if(dot(on_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
         return on_unit_sphere;
     else
         return -on_unit_sphere;
+}
+
+inline vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2*dot(v,n)*n;
+}
+
+inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+    auto cos_theta = std::fmin(dot(-uv,n), 1.0);
+    vec3 r_out_perp = etai_over_etat * (uv + cos_theta*n);
+    vec3 r_out_parallel = -std::sqrt(std::fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_perp + r_out_parallel;
 }
 
 #endif
