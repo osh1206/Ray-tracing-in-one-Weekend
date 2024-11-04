03efcdde8d9c8d4573b431f4fc26795e7a67f171
root <root@BOOK-G99JFJOUSP>
2024-11-04 13:03:01 +0900

    10.2

---
+++ src/material.h
@@ -0,0 +1,18 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+#include "hittable.h"
+
+class material {
+    public:
+        virtual ~material() = default;
+
+        virtual bool scatter(
+            const ray& r_in, const hit_recort& rec, color& attenuation, ray& scattered
+        ) const {
+            return false;
+        }
+        
+}
+
+#endif
